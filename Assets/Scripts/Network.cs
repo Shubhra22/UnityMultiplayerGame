7ab1bd469db56ad6c17cb42727e1c4d8041e263d
Shubhra <ssuvro22@outlook.com>
2020-10-06 21:32:02 -0700

    Set up connection with server

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+[RequireComponent(typeof(SocketIOComponent))]
+public class Network : MonoBehaviour
+{
+    private SocketIOComponent _socket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _socket = GetComponent<SocketIOComponent>();
+        _socket.On("open",Callback);
+    }
+
+    private void Callback(SocketIOEvent obj)
+    {
+        Debug.Log("connected");
+        _socket.Emit("move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
